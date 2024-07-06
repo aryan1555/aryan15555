57feec70febec5587795695dd6bdf0d987456664
bansi43 <bansiundhad34@gmail.com>
2024-07-06 11:59:37 +0530

    chang

--- cpp/hotelmenu.cpp
+++ cpp/hotelmenu.cpp
@@ -1,14 +1,34 @@
 #include<iostream>
  using namespace std;
 
- class menu {
+ class hotel {
 
-    private:
+    protected:
+    int menu[10];
 
-    int name;
-    int number;
-    int food;
-    int qty;
-    
+ };
 
+
+
+ int main (){
+
+
+   hotel h;
+   cout<<"o*o*o*o*o*o*o*o*o* Hotel Pet Puja *o*o**o*o*o*o*o*o*o*o*o"<<endl<<endl;
+   
+              cout<<"        <<<<<<<<<<Food Menu>>>>>>>>>>>>"   <<endl<<endl;
+
+     cout<<"                :SANDWICH:        "<<endl<<endl;
+
+
+      cout<<"No.              Food Dish               Price "<<endl;
+
+      cout<<"1.              VEG.SANDWICH              ₹150/-  "<<endl;
+      cout<<"2.        VEG.CHEES SANDWICH              ₹250/-  "<<endl;
+      cout<<"3.        VEG.PANIR SANDWICH              ₹350/-  "<<endl;
+      cout<<"4.      VEG.ITALIYANSANDWICH              ₹450/-  "<<endl;
+      cout<<"5.          SP. VEG.SANDWICH              ₹550/-n   "<<endl;
+
+
+   return 0;
  }
